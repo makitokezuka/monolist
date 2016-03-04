40d7de97dc933e2cccffffa6df7bedc28f6dbf8b
makitokezuka <kezuka@media-cf.co.jp>
2016-03-04 08:24:41 +0000

    want /have button

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,16 +11,12 @@ class OwnershipsController < ApplicationController
 
     # itemsテーブルに存在しない場合はAmazonのデータを登録する。
     if @item.new_record?
-
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = Amazon::Ecs.item_search(params[:asin],
-                                          :search_index => 'All',
-                                          :response_group => 'Medium',
-                                          :country => 'jp')
-      @amazon_items = response.items
+        response = Amazon::Ecs.item_lookup(params[:asin],{:response_group => 'Medium' , :country => 'jp'})
+
       rescue Amazon::RequestError => e
-        return render :js => "alert('#{e.message}')"
+      return render :js => "alert('#{e.message}')"
       end
       amazon_item       = response.items.first
       @item.title        = amazon_item.get('ItemAttributes/Title')
@@ -33,19 +29,22 @@ class OwnershipsController < ApplicationController
       
     end
 
-    # TODO ユーザにwant or haveを設定する
-    if params[type: "Want"]
-      puts "want"
+    # ユーザにwant or haveを設定する
+    if params[:type]=="Want"
       current_user.want(@item)
-    end
+      puts "finished want"
+      
+    elsif params[:type]=="Have"
+      current_user.have(@item)
+      puts "finished have"
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
-    
-  redirect_to root_path
+    end
   end
 
   def destroy
     @item = Item.find(params[:item_id])
+    unhave(@item)
 
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
@@ -54,6 +53,6 @@ class OwnershipsController < ApplicationController
   end
   
   def index
-    @ownerships = Ownership.all
+    @ownerships = Ownership.all.order(created_at: :desc)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -37,27 +37,27 @@ class User < ActiveRecord::Base
 
   ##want/have 実装
   def have(item)
-    have_items.create(item_id: item.id)
+    haves.create(item_id: item.id)
   end
 
   def unhave(item)
-    have_items.find_by(item_id: item.id).destroy
+    haves.find_by(item_id: item.id).destroy
   end
 
   def have?(item)
-    have_items.include?(item)
+    haves.include?(item)
   end
 
   def want(item)
-    want_items.create(item_id: item.id)
+    wants.create(item_id: item.id)
   end
 
   def unwant(item)
-    want_items.find_by(item_id: item.id).destroy
+    wants.find_by(item_id: item.id).destroy
   end
 
   def want?(item)
-    want_items.include?(item)
+    wants.include?(item)
   end
   
 
