ebde78f91a7a98541510a7bcba8449bf68d961f6
makitokezuka <kezuka@media-cf.co.jp>
2016-03-09 06:30:08 +0000

    for test order by group

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,14 +1,15 @@
 class RankingController < ApplicationController
     
 def have
+    @rankings = Ownership.where(type: "Want").group(:item_id)limit(10).order('count_item_id desc').count('item_id').keys
     
-    @items = Item.all
-    @items.each do |item|
-        @item = Item.find(item.id)
-        @item.have_point = item.have_users.count
-        @item.save
-    end
-    @rankings = Item.order("have_point desc").limit(10)
+    #@items = Item.all
+    #@items.each do |item|
+    #    @item = Item.find(item.id)
+    #    @item.have_point = item.have_users.count
+    #    @item.save
+    #end
+    #@rankings = Item.order("have_point desc").limit(10)
 end
 
 def want
