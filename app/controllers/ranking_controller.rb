75b173939ee18f57a82efd10d9b175a420f60a00
makitokezuka <kezuka@media-cf.co.jp>
2016-03-09 07:28:18 +0000

    have apply

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,26 +1,14 @@
 class RankingController < ApplicationController
     
 def have
-    @rankings = Ownership.where(type: "Want").group(:item_id)limit(10).order('count_item_id desc').count('item_id').keys
-    
-    #@items = Item.all
-    #@items.each do |item|
-    #    @item = Item.find(item.id)
-    #    @item.have_point = item.have_users.count
-    #    @item.save
-    #end
-    #@rankings = Item.order("have_point desc").limit(10)
+    @rankings = Ownership.where(type: "Have").group(:item_id).limit(10).order('count_item_id desc').count('item_id').keys
+
+
 end
 
 def want
-        
-    @items = Item.all
-    @items.each do |item|
-        @item = Item.find(item.id)
-        @item.want_point = item.want_users.count
-        @item.save
-    end
-    @rankings = Item.order("want_point desc").limit(10)
+    @rankings = Ownership.where(type: "Want").group(:item_id).limit(10).order('count_item_id desc').count('item_id').keys
+
 end
 
 
